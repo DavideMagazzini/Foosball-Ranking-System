8017c185a1a2948a39d31f29d529daa95dcdb1bb
MAGAZD <davide_magazzini_akronos@whirlpool.com>
2025-07-30 11:13:48 +0200

    Added addplayerbutton

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from database.DatabaseWrapper import DatabaseWrapper
+from models.player import Player
 app = Flask(__name__)
+
+db_wrap = DatabaseWrapper()
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route('/esegui-azione', methods=['POST'])
+def esegui_azione():
+    # Il tuo codice Python viene eseguito qui
+    print("Pulsante premuto! Eseguo la funzione Python tramite JavaScript.")
+    db_wrap.addPlayer(Player('Prima', 'Prova'))
+    
+    messaggio_di_successo = {"status": "OK", "messaggio": "Azione eseguita con successo!"}
+    return jsonify(messaggio_di_successo)
 
 if __name__ == "__main__":
     app.run(debug=True)
