bf66f2951f4449b325e34a4a06e073d38fc22286
MAGAZD <davide_magazzini_akronos@whirlpool.com>
2025-09-10 17:50:27 +0200

    Added methods to compute stats after game and upload them

--- models/player.py
+++ models/player.py
@@ -1,4 +1,5 @@
 from models.score import Score
+from models.stats import Stats
 from dataclasses import dataclass, field
 from bson import ObjectId
 
@@ -10,6 +11,7 @@ class Player:
     atk_score: Score | dict = field(default_factory=Score) 
     prev_def_score: Score = field(default_factory=Score)
     prev_atk_score: Score = field(default_factory=Score)
+    stats: Stats | dict = field(default_factory=Stats)
     _id: ObjectId | str = None  # Optional, set when loaded from DB
 
     def __post_init__(self):
@@ -24,3 +26,6 @@ class Player:
             self.prev_def_score = Score(**self.prev_def_score)
         if isinstance(self.prev_atk_score, dict):
             self.prev_atk_score = Score(**self.prev_atk_score)
+        if isinstance(self.stats, dict):
+            self.stats = Stats(**self.stats)
+
